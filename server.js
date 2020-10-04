c26e02dcc762225e77bc35fbdff6435873a38796
Matthew Whitehurst <mwhiteh8@uncc.edu>
2020-10-04 17:56:58 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,22 +4,23 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 375
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+var budget = require('./server.json');
+// const budget = {
+//     myBudget: [
+//         {
+//             title: 'Eat out',
+//             budget: 25
+//         },
+//         {
+//             title: 'Rent',
+//             budget: 375
+//         },
+//         {
+//             title: 'Grocery',
+//             budget: 110
+//         },
+//     ]
+// };
 
 app.get('/hello', (req, res) => { 
     res.send('Hello World!');
