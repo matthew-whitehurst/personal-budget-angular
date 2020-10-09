b66b57faaae7b85ab41f65de079f673daf493151
Matthew Whitehurst <mwhiteh8@uncc.edu>
2020-10-09 16:44:06 -0400

    Angular Personal Budget Application

--- personal-budget/src/app/app-routing.module.ts
+++ personal-budget/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { P404Component } from './p404/p404.component';
+import { HeroComponent } from './hero/hero.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
@@ -18,6 +20,14 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'hero',
+    component: HeroComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
   {
     path: '**',
